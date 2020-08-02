e4db006ab44fb3626143e3b9e714550316c99c7d
Barak <barakil109@gmail.com>
2020-08-02 20:32:33 +0300

    create react app command

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Welcome from "./screens/Welcome";
+import Swipes from "./screens/Swipes";
+import Closet from "./screens/Closet";
+import Validation from "./screens/Validation";
+import "./App.css";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
